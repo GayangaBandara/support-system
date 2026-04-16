f3b140feb14f8887446c705c0ea547958893043d
GayangaBandara <gayangabandara74@gmail.com>
2026-04-16 11:54:41 +0530

    Add Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ticket Routes
+Route::get('/tickets/search', [TicketController::class, 'search'])->name('tickets.search');
+Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
+Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
