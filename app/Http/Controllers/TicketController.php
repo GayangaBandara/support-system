92a1fc1c82a256dee14dd2bc487629682e74636b
GayangaBandara <gayangabandara74@gmail.com>
2026-04-16 12:12:01 +0530

    Add Show Function

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
 
 class TicketController extends Controller
 {
-    //
+    //Show Ticket
+    public function show($id)
+    {
+        $ticket = Ticket::findOrFail($id);
+
+        return view('tickets.show', compact('ticket'));
+    }
+
 }
