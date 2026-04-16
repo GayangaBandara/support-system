470421623ca8dc77c57bbac37b7c0bc5dbc171a0
GayangaBandara <gayangabandara74@gmail.com>
2026-04-16 12:54:03 +0530

    Add Search Form

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<form action="{{ route('tickets.search') }}" method="get">
+    <input type="text" name="reference" placeholder="Enter reference">
+    <button type="submit">Search Ticket</button>
+</form>
